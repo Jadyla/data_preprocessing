c1031d230d975433cfb7da9aafeff63f1f5d21bf
Jadyla <jadylacesariof@gmail.com>
2022-07-06 00:09:38 -0300

    Amostragem de dados

--- preProcessamento.R
+++ preProcessamento.R
@@ -1,11 +1,10 @@
-install.packages("e1071")
+#install.packages("e1071")
 library("tidyverse")
 library("readr")
 library("dplyr")      #pacote utilizado para o 'select()'
 library("tidyselect") #pacote para usar o 'everything()', que seleciona todos os atributos
 library("e1071")      #pacote para calcular obliquidade e curtose
 library("base")
-
 
 data <- read_csv("data.csv")
 
@@ -158,6 +157,8 @@ amplitude <- function(atributo){
 
 #TÓPICO 6 - MEDIDAS DE DISTRIBUIÇÃO
 #chamando a função passando um atributo
+
+#atributos quantitativos
 curtose(shots$lat)
 obliq(shots$lat)
 distrib(shots$lat, "lat")
@@ -193,6 +194,24 @@ obliq(shots$seconds_remaining)
 distrib(shots$seconds_remaining, "seconds_remaining")
 normal(shots$seconds_remaining, "seconds_remaining")
 
+#atributos qualitativos
+frequencia(shots$action_type, "action_type")
+frequencia(shots$combined_shot_type, "combined_shot_type")
+frequencia(shots$game_event_id, "game_event_id")
+frequencia(shots$game_id, "game_id")
+frequencia(shots$playoffs, "playoffs")
+frequencia(shots$season, "season")
+frequencia(shots$shot_type, "shot_type")
+frequencia(shots$shot_zone_area, "shot_zone_area")
+frequencia(shots$shot_zone_basic, "shot_zone_basic")
+frequencia(shots$team_id, "team_id")
+frequencia(shots$team_name, "team_name")
+frequencia(shots$matchup, "matchup")
+frequencia(shots$opponent, "opponent")
+frequencia(shots$shot_id, "shot_id")
+frequencia(shots$period, "period")
+frequencia(shots$shot_zone_range, "shot_zone_range")
+frequencia(shots$game_date, "game_date")
 
 curtose <- function(atributo){
   kurtosis(atributo)
@@ -224,9 +243,43 @@ normal <- function(atributo, eixoXName){
     geom_vline(xintercept = desvio, color = "blue")
 }
 
+#funcao para atributos qualitativos
+frequencia <- function(atributo, eixoXName){
+  x <- table(atributo)
+  barplot(x,
+          xlab = eixoXName,
+          ylab = "Frequência")
+}
+
 
 
 #TÓPICO 7 - SEPARANDO OS CASOS TESTE
-teste <- shots[sample(nrow(shots), 6000), ]
-?sample
-?select()
+#selecionando aleatoriamente os casos teste
+teste <- shots[sample(nrow(shots), 6000), replace = FALSE]
+#separando os casos de treinamento a partir da diferença entre o teste e 'shots'
+shotsTraining <- setdiff(shots, teste)
+
+
+
+#TÓPICO 8 e 9 (em partes) - ELIMINAÇÃO DE ATRIBUTOS
+shotsTraining$game_event_id <- NULL
+shotsTraining$game_id <- NULL
+shotsTraining$team_id <- NULL
+shotsTraining$team_name <- NULL
+shotsTraining$shot_id <- NULL
+shotsTraining$matchup <- NULL
+
+teste$game_event_id <- NULL
+teste$game_id <- NULL
+teste$team_id <- NULL
+teste$team_name <- NULL
+teste$shot_id <- NULL
+teste$matchup <- NULL
+
+
+#TÓPICO 10 - AMOSTRAGEM
+shotsTraining <- shotsTraining[sample(nrow(shotsTraining), 10000), replace = FALSE]
+teste <- teste[sample(nrow(teste), 2430), replace = FALSE]
+
+table(shotsTraining$shot_made_flag)
+table(teste$shot_made_flag)
